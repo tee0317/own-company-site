d038e6b875f65142c53775a79e8b74962cbec52c
TiE <kbetmhk@gmail.com>
2026-07-23 15:47:51 +0900

    baseパスの末尾スラッシュ不足によるリンク切れを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kbetmhk.github.io',
-  base: '/own-company-site',
+  base: '/own-company-site/',
 });
